466e307b9f61d571fe5e0d1faca062abb60a6e95
LaisBlum <78423708+LaisBlum@users.noreply.github.com>
2024-07-09 11:24:11 -0300

    Methods

--- poo/classe_data.dart
+++ poo/classe_data.dart
@@ -3,6 +3,14 @@ class Data {
   late int dia;
   late int mes;
   late int ano;
+
+  String obterFormatada(){
+    return "$dia/$mes/$ano";
+  }
+
+  String toString(){
+    return obterFormatada();
+  }
 }
 
 main(){
@@ -14,8 +22,18 @@ main(){
   var dataCompra = Data(); // new não é obrigatório
   dataCompra.dia = 23;
   dataCompra.mes = 12;
-  dataCompra.mes = 2021;
+  dataCompra.ano = 2021;
 
-  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
-  print("${dataCompra.dia}/${dataCompra.mes}/${dataAniversario.ano}");
+  // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  // print("${dataCompra.dia}/${dataCompra.mes}/${dataAniversario.ano}");
+  String d1 = dataAniversario.obterFormatada();
+
+  print("A data do aniversário é $d1");
+  print("A data da compra é ${dataCompra.obterFormatada()}");
+
+  print(dataCompra); // = print(dataCompra.toString()); por padrão, retorna "Instance of <classe>", mas posso reescrever o método
+
+  // Nesse caso dataCompra != dataCompra.toString();
+  Data d2 = dataCompra;
+  String s1 = dataCompra.toString();
 }
